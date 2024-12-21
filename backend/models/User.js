7dd4f217c1d6d9f69c0a8d20fcfc54658e06cc2f
anuradha <anuradhasanjaya2024@gmail.com>
2024-12-21 13:39:27 +0530

    finalized the backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,15 +8,4 @@ const UserSchema = new mongoose.Schema({
   role: { type: String, enum: ['admin', 'loan_officer'], required: true },
 });
 
-// Pre-save middleware to hash password
-// UserSchema.pre('save', async function(next) {
-//   if (!this.isModified('password')) next();
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
-
-// UserSchema.methods.matchPassword = async function(enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
 module.exports = mongoose.model('User', UserSchema);
